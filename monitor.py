78ad285305beb62ce3cd239d890b27f7c49c3fcd
ArtyomPeterson <126898068+ArtyomPeterson@users.noreply.github.com>
2026-01-18 20:36:20 +0100

    Create monitor.py
    Create the monitoring script

---
+++ monitor.py
@@ -0,0 +1,23 @@
+import requests, hashlib, os
+
+URL = "https://dvprogram.state.gov/"
+BOT_TOKEN = os.environ["7766675410:AAE-lYf9QE1nh-wEOCPfkP68kT3iUFYNJys"]
+CHAT_ID = os.environ["484284070"]
+
+def send(text):
+    requests.post(
+        f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage",
+        data={"chat_id": CHAT_ID, "text": text}
+    )
+
+r = requests.get(URL, timeout=20)
+h = hashlib.md5(r.text.encode()).hexdigest()
+
+try:
+    old = open("hash.txt").read()
+except:
+    old = ""
+
+if h != old:
+    send("🚨 DV Lottery site updated! https://dvprogram.state.gov/")
+    open("hash.txt","w").write(h)
